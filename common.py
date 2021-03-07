aeee4d7675b0aec6c8c0defdff9b050a15e99271
stealthmate <stealthmate1233@gmail.com>
2021-03-08 03:38:05 +0900

    Init

---
+++ common.py
@@ -0,0 +1,68 @@
+import twitter
+from time import sleep
+from datetime import datetime, date, timedelta
+import json
+import os
+import pytz
+
+LIST_ID = os.environ['TWEETTRASH_LIST_ID']
+CONSUMER_KEY = os.environ['TWEETTRASH_CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['TWEETTRASH_CONSUMER_SECRET']
+ACCESS_TOKEN_KEY = os.environ['TWEETTRASH_ACCESS_TOKEN_KEY']
+ACCESS_TOKEN_SECRET = os.environ['TWEETTRASH_ACCESS_TOKEN_SECRET']
+DATETIME_FORMAT = '%a %b %d %H:%M:%S %z %Y'
+OUTPUT_DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S %z'
+
+TZ = pytz.timezone('Asia/Tokyo')
+
+def init_api():
+    return twitter.Api(
+        consumer_key=CONSUMER_KEY,
+        consumer_secret=CONSUMER_SECRET,
+        access_token_key=ACCESS_TOKEN_KEY,
+        access_token_secret=ACCESS_TOKEN_SECRET
+    )
+
+def time_created(status):
+    return datetime.strptime(status.created_at, DATETIME_FORMAT).astimezone(tz=TZ)
+
+def get_prev_day_statuses(api=None):
+    if api is None:
+        api = init_api()
+
+    today = date.today()
+    target = date.today() - timedelta(days=1)
+    since = target - timedelta(days=1)
+
+    theresults = []
+    kwargs = {
+        'list_id': LIST_ID,
+        'include_rts': False,
+        'count': 200
+    }
+    results = api.GetListTimeline(**kwargs)
+    maxid = -1
+    total = 0
+    while len(results) > 1 and time_created(results[0]).date() > since:
+        theresults.extend(results)
+        maxid = results[-1].id
+        results = api.GetListTimeline(max_id=maxid, **kwargs)
+        print(f"Got {len(results)} tweets!")
+        sleep(1)
+    return [x for x in theresults if time_created(x).date() == target]
+
+def post_tweet(txt, api=None):
+    if not api:
+        api = init_api()
+    api.PostUpdate(txt)
+
+
+def statuses_to_json(xs, f):
+    open(f, mode='w').write(
+        json.dumps([
+            {
+                'text': x.text,
+                'created': time_created(x).strftime(OUTPUT_DATETIME_FORMAT),
+                'user': x.user.name
+            } for x in xs
+        ], ensure_ascii=False))
---
+++ post_daily_update.py
@@ -0,0 +1,39 @@
+import common
+import pandas as pd
+from datetime import date, timedelta
+
+def run():
+    api = common.init_api()
+    statuses = common.get_prev_day_statuses(api=api)
+    day = date.today() - timedelta(days=1)
+
+    df = pd.DataFrame([
+        {
+            'time': common.time_created(s),
+            'user': s.user.name,
+            'userid': s.user.screen_name,
+            'text': s.text
+        } for s in statuses
+    ])
+
+    total = df.shape[0]
+    by_user = df.groupby(['user', 'userid'])['text'].count()
+    mvp = by_user.idxmax()
+    mvp_c = by_user.max()
+    mvpbad = by_user.idxmin()
+    mvpbad_c = by_user.min()
+    members = len(by_user)
+
+    post = f'''昨日 ({day.year}-{day.month:02}-{day.day:02}) の活動まとめ：
+総ツイート数: {total} ツイート
+活動者数　　: {members}
+ツイ廃王者　: @{mvp[1]} ({mvp_c} ツイート，全体の {100 * mvp_c / total:.2f}%)
+ビリ廃　　　: @{mvpbad[1]} ({mvpbad_c} ツイート，全体の {100 * mvpbad_c / total:.2f}%)
+'''
+    print(post)
+    print("Posting ...")
+    # common.post_tweet(post, api=api)
+    print("OK!")
+
+if __name__ == '__main__':
+    run()
